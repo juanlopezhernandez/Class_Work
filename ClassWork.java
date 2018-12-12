7e9c2d71e535b83533cb5ee60707d92b1a62ff3e
juanlopezhernandez <lopezjuan0192@gmail.com>
2018-12-12 15:35:22 -0800

    user input

---
+++ ClassWork.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ClassWork {
+	
+ public static void main(String []args){
+
+	        
+	Scanner input = new Scanner(System.in);
+	        
+	        
+	System.out.println("Please state your age: ");
+	    
+	        
+	int value = input.nextInt();
+	        
+	   if (value <= 17){
+	       System.out.println("You are too young to get a tatoo.");
+	       System.out.println("You are too young to drink alcohol.");
+	       System.out.println("You are too young to run for U.S.");
+	    }  else {
+	       System.out.println("Do what your heart desires.");
+	         }
+	   }
+	
+
+}
